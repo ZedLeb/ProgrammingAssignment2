a9494f9d7acd76066638870b8026db9b38de6ce1
Zoe Lebourgeois <zoe.lebourgeois@yahoo.co.uk>
2014-09-16 23:17:53 +0200

    This is my commit from the Git Gui (Git Bash/Vim didn't work)

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+#This is my go at making a change recognised in
+#GIT
+## I can at least get some points for committing to GitHub :)
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
